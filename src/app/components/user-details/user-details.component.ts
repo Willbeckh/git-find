24f50bdde466be651254a141823fa8d97caa66d0
Willbeckh <wileyhndirangu04@gmail.com>
2022-04-20 17:20:03 +0300

    ft: add content for searched user details

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { GithubService } from 'src/app/services/github.service';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css']
+  styleUrls: ['./user-details.component.css'],
 })
 export class UserDetailsComponent implements OnInit {
+  username!: string;
+  userDetail: any;
+  repoData!: any;
 
-  constructor() { }
+  constructor(
+    private active: ActivatedRoute,
+    private githubService: GithubService,
+    private route: Router
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.active.params.subscribe((params) => {
+      this.username = params['id'];
+      console.log('parameter', this.username);
+    });
 
+    // get user details
+    this.githubService.getUser(this.username).then((data) => {
+      this.userDetail = data;
+      console.log('userDetail', typeof this.userDetail);
+    });
+
+    // gets the repos of the user
+    this.githubService.getRepos(this.username).then((data) => {
+      this.repoData = data;
+      console.log('repoData', this.repoData);
+    });
+  }
 }
