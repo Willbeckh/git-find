8d7cd694f67dff0c1f4fb9f05d9e31922ea74f31
Willbeckh <wileyhndirangu04@gmail.com>
2022-04-20 17:21:57 +0300

    ft: create repository class interface

---
+++ src/app/repository.ts
@@ -0,0 +1,10 @@
+export class Repository {
+  constructor(
+    name: string,
+    html_url: string,
+    description: string,
+    updated_at: Date,
+    created_at: Date,
+    clone_url: string
+  ) {}
+}
