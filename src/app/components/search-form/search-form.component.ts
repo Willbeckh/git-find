db18d918f19bc3b218b35eb9f62485d2599a08c3
Willbeckh <wileyhndirangu04@gmail.com>
2022-04-20 17:18:57 +0300

    ft: create user input search form logic

--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -1,32 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.css']
+  styleUrls: ['./search-form.component.css'],
 })
 export class SearchFormComponent implements OnInit {
-
   searchForm!: FormGroup;
   username!: string;
 
-  constructor() { }
+  constructor(private route: Router) {}
 
   ngOnInit(): void {
     this.searchForm = new FormGroup({
-      username: new FormControl(
-        null,
-        [Validators.required]
-      ),
+      username: new FormControl(null, [Validators.required]),
     });
-
   }
 
   // method to get the username from the form
-  searchUsername(){
-    // console.log(this.searchForm.value);
+  searchUsername() {
     this.username = this.searchForm.value.username;
-    // return this.searchForm.get('username').value;
-  } 
+    this.route.navigate([`user/${this.username}`]);
+  }
 }
