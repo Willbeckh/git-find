ea5b3cd3377e8e8e3b0d20395ab8d3b0223b902e
Willbeckh <wileyhndirangu04@gmail.com>
2022-04-20 17:20:39 +0300

    ch: add api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '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',
 };
 
 /*
