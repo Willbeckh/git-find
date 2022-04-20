119a9aa32c70f22e214a82b1a77655268108a582
Willbeckh <wileyhndirangu04@gmail.com>
2022-04-20 22:43:06 +0300

    ch: update module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { SearchFormComponent } from './components/search-form/search-form.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import { SearchFormComponent } from './components/search-form/search-form.compon
     HomeComponent,
     UserDetailsComponent,
     SearchFormComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,6 +28,7 @@ import { SearchFormComponent } from './components/search-form/search-form.compon
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
